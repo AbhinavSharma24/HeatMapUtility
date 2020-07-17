b1b09d61f93c9e4116964a63539b02081e76904b
abhinav <abhinavsharma24999@gmail.com>
2020-07-17 15:55:20 +0530

    Hotspot Details Marker

--- app/src/main/java/com/example/heatmaputility/Main2Activity.kt
+++ app/src/main/java/com/example/heatmaputility/Main2Activity.kt
@@ -1,14 +1,19 @@
 package com.example.heatmaputility
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -17,6 +22,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.TileOverlay
 import com.google.android.gms.maps.model.TileOverlayOptions
 import com.google.maps.android.heatmaps.Gradient
@@ -33,6 +39,8 @@ class Main2Activity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
 
     private lateinit var locMan: LocationManager
     private lateinit var locLis: LocationListener
+
+    private var pro = 0
 
     private var mDefaultGradient = true
 
@@ -52,7 +60,7 @@ class Main2Activity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
             ALT_HEATMAP_GRADIENT_START_POINTS)
 
     override fun onLocationChanged(location: Location?) {
-        Toast.makeText(this,"lat = ${location?.latitude}    long = ${location?.longitude}",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,"lat = ${location?.latitude} long = ${location?.longitude}",Toast.LENGTH_SHORT).show()
         if(mMap != null){
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location?.latitude?.let {
                 LatLng(
@@ -110,31 +118,83 @@ class Main2Activity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
 
     private lateinit var mProvider : HeatmapTileProvider
     private lateinit var mOverlay : TileOverlay
-
+    private var zoom = -1f
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val myMarker: MarkerOptions = MarkerOptions().position(LatLng(28.633159, 80.219613)).title("First")
+        mMap.addMarker(myMarker)
+        val center = LatLng(28.633159, 80.219613)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 4f))
+
+        mMap.setOnMarkerClickListener {
+            val tag = it.title
+            if(tag.equals("First", true)) {
+                Toast.makeText(this, "This is a hotspot!", Toast.LENGTH_SHORT).show()
+                val dialog = Dialog(this)
+
+                dialog.setCancelable(false)
+                dialog.setContentView(R.layout.hotspot_details)
+
+                val text: TextView = dialog.findViewById(R.id.text_dialog) as TextView
+
+                dialog.setCanceledOnTouchOutside(false)
+
+                val cancelCommentDelete: ImageView = dialog.findViewById(R.id.cancel_comment_delete)
+
+                cancelCommentDelete.setOnClickListener { dialog.dismiss() }
+
+                dialog.show()
+
+            } else {
+                Toast.makeText(this, "Wrong hotspot!", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
 
         with(mMap.uiSettings) {
             setAllGesturesEnabled(true)
             isZoomControlsEnabled = true
         }
         
-//        val newMap = mMap.z
-
         addHeatMapWeighted()
         //addHeatMap();
     }
 
     private fun addHeatMapWeighted() {
-        val list = readItems(R.raw.sample_data)
-        val provider = HeatmapTileProvider.Builder().weightedData(list).gradient(ALT_HEATMAP_GRADIENT).build()
-        mMap.addTileOverlay(TileOverlayOptions().tileProvider(provider))
+        val list = readItems(R.raw.co_csvjson)
+        mProvider = HeatmapTileProvider.Builder().gradient(HeatmapTileProvider.DEFAULT_GRADIENT).weightedData(list).build()
+        mMap.addTileOverlay(TileOverlayOptions().tileProvider(mProvider))
+//        pro = gradientChange(R.raw.sample_data)
+
+        /*val inputStream: InputStream = resources.openRawResource(R.raw.sample_data)
+        val json: String = Scanner(inputStream).useDelimiter("\\A").next()
+        val array = JSONArray(json)
+        for (i in 0 until array.length()) {
+            val `object` = array.getJSONObject(i)
+            val id = `object`.getInt("id")
+            if (id == 1) {
+                mProvider.setGradient(HeatmapTileProvider.DEFAULT_GRADIENT)
+            } else if (id == 2) {
+                mProvider.setGradient(ALT_HEATMAP_GRADIENT)
+            }
+        }*/
+
+        val list2 = readItems(R.raw.o3_csvjson)
+        mProvider = HeatmapTileProvider.Builder().gradient(ALT_HEATMAP_GRADIENT).weightedData(list2).build()
+        mMap.addTileOverlay(TileOverlayOptions().tileProvider(mProvider))
+
+        zoom = mMap.cameraPosition.zoom
+
+        if(zoom < 5f){
+            mProvider.setRadius(20)
+        } else{
+            mProvider.setRadius(100)
+        }
     }
+
+
 
     /*fun addHeatMap() {
         val gradient = Gradient(colors, startpoints)
@@ -179,6 +239,23 @@ class Main2Activity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
         mOverlay.clearTileCache()*//*
     }*/
 
+    /*private fun gradientChange(resource: Int): Int {
+        var value = 0
+        val inputStream: InputStream = resources.openRawResource(resource)
+        val json: String = Scanner(inputStream).useDelimiter("\\A").next()
+        val array = JSONArray(json)
+        for (i in 0 until array.length()) {
+            val `object` = array.getJSONObject(i)
+            val id = `object`.getInt("id")
+            if(id == 1){
+                value = 1
+            }else if(id == 2){
+                value = 2
+            }
+        }
+        return value
+    }*/
+
     @Throws(JSONException::class)
     private fun readItems(resource: Int): ArrayList<WeightedLatLng?>? {
         val list = ArrayList<LatLng?>()
@@ -190,6 +267,7 @@ class Main2Activity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
             val `object` = array.getJSONObject(i)
             val lat = `object`.getDouble("lat")
             val lng = `object`.getDouble("long")
+
             var weig: Double? = null
             try {
                 weig = `object`.getDouble("weight")
@@ -200,12 +278,12 @@ class Main2Activity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
 
             val x : LatLng? = list[i]
 
-            val weight = weig?.times(1000)
+            val weight = weig?.times(100)
 
             if (weight != null) {
-                if(weight < 20){
+                if(weight < 12){
                     list2.add(WeightedLatLng(x, 1.0))
-                }else if(weight < 30 && weight >= 20){
+                }else if(weight < 30 && weight >= 12){
                     list2.add(WeightedLatLng(x, 10.0))
                 }else if(weight < 50 && weight >= 30){
                     list2.add(WeightedLatLng(x, 50.0))
